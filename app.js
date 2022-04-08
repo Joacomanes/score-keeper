15f8ed855b897f152efce1f4f76e171ed04cb6e4
Joacomanes <joacomanes@gmail.com>
2022-04-08 20:27:56 -0300

    Added variables

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Howdy!");
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
+
+let p1Score = 0;
+let p2Score = 0;
